db5ec161a52df4d8bb6bd07df434f19ca97d4cc1
Gautam@Arch <gautam.nraj@gmail.com>
2025-12-14 20:23:50 +0530

    regular backup 14-12-25

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -1,9 +1,22 @@
 return {
-  "lervag/vimtex",
-  lazy = false,     -- we don't want to lazy load VimTeX
-  -- tag = "v2.15", -- uncomment to pin to a specific release
-  init = function()
-    -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
-  end
+	"lervag/vimtex",
+	lazy = false, -- don't lazy load VimTeX
+	-- tag = "v2.15", -- uncomment if you want to pin a specific release
+	init = function()
+		-- Required for VimTeX to work correctly (usually default in nvim)
+		vim.cmd("filetype plugin indent on")
+		vim.cmd("syntax enable")
+
+		-- Viewer options
+		vim.g.vimtex_view_method = "zathura"
+		-- Alternative generic viewer example (commented)
+		-- vim.g.vimtex_view_general_viewer = "okular"
+		-- vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+
+		-- Compiler backend, use 'latexrun' or the default 'latexmk' (recommended)
+		vim.g.vimtex_compiler_method = "latexmk"
+
+		-- Map localleader to comma, VimTeX mappings use localleader
+		vim.g.maplocalleader = " "
+	end,
 }
